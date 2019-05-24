daee621b46bf3d8987f1047763f66c34ef116011
Xurxe <xurxe.garcia@integrify.io>
2019-05-24 09:11:23 +0300

    Added class notes from 23.may.2019

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
+import { connect } from 'react-redux';
+
+import { addOne, subtractOne, changeName } from './index.js'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const higherOrderFunction1 = (x) => {
+    return (y) => x * 2 * y + 10;
+}
+console.log(higherOrderFunction1(2)(3));
+
+const higherOrderFunction2 = (x, callback) => {
+    return x * callback(3) + 10;
+}
+const callback1 = (y) => {
+    return 2 * y;
+}
+console.log(higherOrderFunction2(2, callback1));
+
+const higherOrderComponent = (Component) => {
+    return (
+        <div>
+            <Component></Component>
+        </div>
+    )
 }
 
-export default App;
+const Welcome = () => <h2>Welcome!</h2>;
+
+class App extends React.Component {
+
+/*     handleAdd = () => {
+        this.setState({count: this.state.count + 1})
+    }
+
+    handleSubtract = () => {
+        this.setState({count: this.state.count - 1})
+    } */
+
+    render() {
+        console.log(this.props);
+        return (
+            <div className="App">
+                <h1>Let's learn Redux</h1>
+                {higherOrderComponent(Welcome)}
+                <h2>Count: {this.props.count}</h2>
+                <button onClick={this.props.addOne}>+1</button>
+                <button onClick={this.props.subtractOne}>-1</button>
+
+            </div>
+        );
+    }
+
+}
+
+const mapStateToProps = (state) => {
+    return {
+        count: state.count,
+        name: state.name,
+        title: state.title,
+    };
+}
+
+const mapActionToProps = (dispatch) => {
+    return {
+        addOne: () => dispatch(addOne()),
+        subtractOne: () => dispatch(subtractOne()),
+        changeName: () => dispatch(changeName()),
+    }
+}
+
+export default connect(mapStateToProps, mapActionToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,108 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import './index.css';
+
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/* const myReducer = (state = 'My initial state') => {
+    if (myAction.type === 'ChangeState') {
+        return myAction.payload;
+    }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    else {
+        return state
+    }
+} */
+
+const initialState = {
+    count: 0,
+    name: 'Xurxe',
+};
+
+const myReducer = (state = initialState, {type, payload}) => {
+    switch (type) {
+        case 'ADD_ONE': {
+            return {...state, count: state.count + 1};
+        }
+
+        case 'SUBTRACT_ONE': {
+            return {...state, count: state.count - 1};
+        }
+
+        case 'CHANGE_NAME': {
+            return {...state, name: payload.name};
+        }
+
+        default: {
+            return state;
+        }
+    };
+};
+
+const actionAdd = {
+    type: 'ADD_ONE',
+};
+
+const actionSubtract = {
+    type: 'SUBTRACT_ONE',
+};
+
+const actionChangeName = {
+    type: 'CHANGE_NAME',
+    payload: {
+        name: 'Asabeneh'
+    },
+};
+
+const myStore = createStore(myReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+/* console.log(myStore);
+console.log(myStore.getState()); */
+
+myStore.dispatch(actionAdd);
+/* console.log(myStore.getState()); */
+
+myStore.dispatch(actionSubtract);
+/* console.log(myStore.getState()); */
+
+myStore.dispatch(actionChangeName);
+/* console.log(myStore.getState()); */
+
+export const addOne = () => {
+    return {
+        type: 'ADD_ONE'
+    }
+};
+
+export const subtractOne = () => {
+    return {
+        type: 'SUBTRACT_ONE'
+    }
+};
+
+export const changeName = (name) => {
+    return {
+        type: 'CHANGE_NAME',
+        payload: {
+            name: name
+        }
+    }
+};
+
+myStore.dispatch(addOne());
+/* console.log(myStore.getState()); */
+
+myStore.dispatch(subtractOne());
+/* console.log(myStore.getState()); */
+
+myStore.dispatch(changeName('Xurxe'));
+/* console.log(myStore.getState()); */
+
+ReactDOM.render(
+    <Provider store = {myStore}>
+        <App title='I am the title'/>
+    </Provider>
+    , document.getElementById('root')
+);
